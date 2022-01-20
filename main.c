7a75db648b393467269baeaafc7b77c646f8ef2b
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2022-01-20 19:14:23 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	printf("Hello, world!\n");
+	printf("Привіт, світ!");
+
+	return 0;
+}
